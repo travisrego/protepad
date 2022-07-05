0dd764ff92f0d86c5b22b5ac525a974dbe4f5d3b
Travis <ms.anitarego@gmail.com>
2022-07-05 17:41:11 +0300

    Fixed Protepad project

---
+++ FindDialog/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace FindDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindNextButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+    }
+}
